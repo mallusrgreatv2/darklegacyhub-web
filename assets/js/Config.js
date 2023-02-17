cf056bb29c5e87195509e0fa309b0c37f046f942
mallusrgreat <freedomgamingyt@gmail.com>
2023-02-17 17:26:34 +0530

    A

---
+++ assets/js/Config.js
@@ -0,0 +1,42 @@
+const Config = {
+  /* START CONFIG */
+
+  "server-logo": "assets/img/logo.png",
+
+  "server-ip": "darklegacymc.tk",
+
+  "server-status": {
+    "background-color": "#fff659",
+    "text-color": "black",
+    icon: "assets/img/status.png",
+
+    value: "{online} / {max} players connected!",
+    "copy-value": "IP COPIED!",
+  },
+
+  background: {
+    type: "image", // TYPES: IMAGE OR COLOR
+    value: "assets/img/background.jpg",
+    opacity: {
+      color: "black",
+      value: 0.8,
+    },
+  },
+
+  "buttons-style": {
+    "background-color": "#fff659",
+    "text-color": "black",
+  },
+
+  "web-buttons": {
+    Features: {
+      icon: "assets/img/store.png",
+      url: "https://darklegacymc.tk/features",
+    },
+  },
+
+  /* END CONFIG */
+};
+
+/* IGNORE */
+export default Config;
