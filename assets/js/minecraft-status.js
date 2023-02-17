2d4d24f92bc08fbdacbcd8e6e1456a480cbc3bcd
mallusrgreat <freedomgamingyt@gmail.com>
2023-02-17 17:30:40 +0530

    PRetty things

--- assets/js/minecraft-status.js
+++ assets/js/minecraft-status.js
@@ -1,38 +1,31 @@
+const ServerStatus = function (andress, port) {
+  var Global = {};
+  (Global.variables = {}),
+    (Global.variables.api_url = "https://api.mcsrvstat.us/2/"),
+    (Global.variables.server_resolved = undefined);
 
+  if (andress == undefined)
+    return console.error("[ServerStatus] Enter Server Andress.");
 
-const ServerStatus = function(andress, port) {
-	var Global = {};
-		Global.variables = {},
-		Global.variables.api_url = "https://api.mcsrvstat.us/2/",
-		Global.variables.server_resolved = undefined;
+  Global.variables.api_url += andress;
 
+  if (port) Global.variables.api_url += ":" + port;
 
-	if (andress == undefined) return console.error("[ServerStatus] Enter Server Andress.");
+  Global.functions = {};
 
-	Global.variables.api_url += andress;
+  Global.functions.getData = function (callback) {
+    fetch(Global.variables.api_url).then((r) => {
+      r.json().then((d) => {
+        Global.variables.server_resolved = d;
+        if (callback != undefined && typeof callback === "function") {
+          callback(d);
+        } else {
+          console.log("Server Checked");
+        }
+      });
+    });
+    return Global.variables;
+  };
 
-	if (port) Global.variables.api_url += ":" + port;
-
-
-	Global.functions = {};
-
-	Global.functions.getData =  function(callback) {
-		fetch(Global.variables.api_url).then(r => {
-			r.json().then(d => {
-				Global.variables.server_resolved = d;
-				if(callback != undefined && typeof callback === 'function') {
-					callback(d);
-
-				} else {
-					console.log('Server Checked');
-				}
-
-			})
-		});
-		return Global.variables;
-	}
-
-
-
-	return Global.functions;
-}
+  return Global.functions;
+};
